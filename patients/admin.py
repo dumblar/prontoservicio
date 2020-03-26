a93c312a1ea1823644ff9b40489cebd03d9be44f
aramirezp92@gmail.com <aramirezp92@gmail.com>
2020-03-26 10:43:53 -0500

    hola

--- patients/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-
-@admin.register(Patient)
-class PatientAdmin (admin.ModelAdmin):
-    list_display = ('id', 'name', 'age', 'eps', 'company')
-    list_filter = ('eps', 'company')
-    search_fields = ('id', 'name')
--- shipping/serializers.py
+++
@@ -1,4 +0,0 @@
-# Serializers define the API representation.
-from rest_framework import serializers
-
-from .models import *
